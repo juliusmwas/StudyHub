29064895c0a9ee53fa2ff0dfeedb6de3a7e78d8d
juliusmwass <juliusmwass100@gmail.com>
2025-11-28 11:28:58 +0300

    Added the visitors Navbar in the homepage

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,20 +4,26 @@ import { HiMenu } from "react-icons/hi";
 export default function Navbar() {
     return(
         <>
-        <div className="bg-blue-800 flex items-center justify-around ">
+        <div className="bg-gray-200 shadow-sm flex items-center justify-around h-20 ">
             <div>
-                <h1 className="text-2xl font-medium p-5">StudyHub</h1>
+                <h1 className=" text-xl px-4 lg:text-3xl font-bold">Study<span className="text-blue-600">Hub</span></h1>
             </div>
 
-            <div>
-                <ul className="flex items-center">
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Features</a></li>
-                    <li><a href="#">How it Works</a></li>
+            <div className="text-sm lg:text-lg text-gray-600 font-normal">
+                <ul className="flex items-center gap-5">
+                    <li className="hover:text-gray-700 hover:font-medium"><a href="#">About</a></li>
+                    <li className="hover:text-gray-700 hover:font-medium"><a href="#">Features</a></li>
+                    <li className="hover:text-gray-700 hover:font-medium"><a href="#">How it Works</a></li>
                 </ul>
             </div>
             
-            <HiMenu />
+
+            <div className="flex items-center gap-3">
+                <button className="border py-1 px-2 rounded-lg text-lg font-medium cursor-pointer">Login</button>
+                 <button className=" text-blue-600 border border-blue-600 py-1 px-2 rounded-lg text-lg font-bold cursor-pointer">Register</button>
+            </div>
+
+            <HiMenu className="text-2xl lg:hidden"/>
 
 
         </div>
